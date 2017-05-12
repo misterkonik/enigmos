d6dfdc77c018be7af83fa0db4eb6f7419e42ef56
misterkonik <durelnicolas1@gmail.com>
2017-05-12 12:15:38 +0200

    Ajout du constructeur et des attributs

--- Enigmas/SerieDe42EnigmaPanel.cs
+++ Enigmas/SerieDe42EnigmaPanel.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Cpln.Enigmos.Enigmas
 {
-    class SerieDe42EnigmaPanel : EnigmaPanel
+   public class SerieDe42EnigmaPanel : EnigmaPanel
     {
+       private Label lblQuestion;
+       private Label lblQuestionReponse;
+       private Label lblFausseReponse;
+
+        public SerieDe42EnigmaPanel()
+        {
+            lblQuestion = new Label();
+            lblQuestion.Text = "Cliquez sur ";
+            lblQuestion.Location = new System.Drawing.Point(0, 0);
+            lblQuestion.Width = 10;
 
 
+            lblQuestionReponse = new Label();
+            lblQuestionReponse.Text = "42";
+            lblQuestionReponse.Location = new System.Drawing.Point(100, 0);
+
+
+            Controls.Add(lblQuestion);
+            Controls.Add(lblQuestionReponse);
+        }
     }
 }
