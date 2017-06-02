9b2a023bdea203f742a2e4061639986ae135f70d
misterkonik <durelnicolas1@gmail.com>
2017-06-02 12:15:44 +0200

    Changer énigme de base

--- Enigmas/SerieDe42EnigmaPanel.cs
+++ Enigmas/SerieDe42EnigmaPanel.cs
@@ -9,32 +9,20 @@ namespace Cpln.Enigmos.Enigmas
 {
    public class SerieDe42EnigmaPanel : EnigmaPanel
     {
-       private Label lblQuestion;
+        private Label lblQuestion;
        private Label lblQuestionReponse;
        private Label lblFausseReponse;
 
         public SerieDe42EnigmaPanel()
         {
-            lblQuestion = new Label();
-            lblQuestion.Text = "Cliquez sur ";
-            lblQuestion.Location = new System.Drawing.Point(0, 0);
-            lblQuestion.Width = 110;
-            lblQuestion.Font = new System.Drawing.Font(lblQuestion.Font.FontFamily,15);
+            lblQuestion = new Label() {Text = "Cliquez sur",Location = new System.Drawing.Point(0, 0),Width = 110,Font = new System.Drawing.Font(Font.FontFamily, 15)};
 
-            lblQuestionReponse = new Label();
-            lblQuestionReponse.Text = "42";
-            lblQuestionReponse.Location = new System.Drawing.Point(105, 0);
+            lblQuestionReponse = new Label() { Text = "42",Location = new System.Drawing.Point(105, 0),Width =35,Font= new System.Drawing.Font(Font.FontFamily, 15)};
             lblQuestionReponse.Click += LblQuestionReponse_Click;
-            lblQuestionReponse.Font = new System.Drawing.Font(lblQuestionReponse.Font.FontFamily, 15);
 
-            lblFausseReponse = new Label();
-            lblFausseReponse.BackColor = System.Drawing.Color.Pink;
-            lblFausseReponse.Width = 800;
-            lblFausseReponse.Height = 500;
-            lblFausseReponse.Location = new System.Drawing.Point(0, 56);
-            lblFausseReponse.Cursor = Cursors.Hand;
+            lblFausseReponse = new Label() {BackColor = System.Drawing.Color.Pink,Width=800,Height=540,Location = new System.Drawing.Point(0, 56),Cursor=Cursors.Hand };
 
-            for(int i = 0; i<20;i++)
+            for(int i = 0; i<21;i++)
             {
                 for (int x = 0; x <53;x++)
                 {
@@ -57,9 +45,8 @@ namespace Cpln.Enigmos.Enigmas
 
             result = MessageBox.Show(message, caption, buttons);
 
-            if (result == System.Windows.Forms.DialogResult.OK)
-            {
-            }
+            if (result == System.Windows.Forms.DialogResult.OK){}
+
         }
     }
 }
